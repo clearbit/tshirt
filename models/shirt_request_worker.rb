c8cf313e75c069e4461c4ecc4a903e8a30b45a67
Alex MacCaw <alex@alexmaccaw.com>
2015-05-12 11:06:36 -0700

    fix invalid domains

--- models/shirt_request_worker.rb
+++ models/shirt_request_worker.rb
@@ -3,7 +3,14 @@ class ShirtRequestWorker
 
   def perform(request_id)
     request = ShirtRequest.first!(id: request_id)
-    lookup  = Clearbit::Streaming::PersonCompany[email: request.email]
+
+    lookup = nil
+
+    begin
+      lookup = Clearbit::Streaming::PersonCompany[email: request.email]
+    rescue URI::InvalidURIError, Nestful::ResourceInvalid
+    end
+
     request.update_all(lookup)
     Mailer.shirt_request!(request)
   end
